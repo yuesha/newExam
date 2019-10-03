01b0d509de403bd79204da674fa1845f122bb3ad
yuesha <2661103378@qq.com>
2019-10-03 17:18:41 +0800

    欢迎页面页面渲染及数据获取、新增数据库模型

--- codeBehind/application/admin/controller/IndexController.php
+++ codeBehind/application/admin/controller/IndexController.php
@@ -1,24 +1,34 @@
 <?php
 namespace app\admin\controller;
 use app\admin\adminBaseClass;
+use app\model\UsersModel as Users;
+use app\model\QuestionsModel as Questions;
+use app\model\ExamModel as Exam;
+// use think\Cache;
 
 /**
  * 后台首页控制器
  */
 class IndexController extends adminBaseClass{
-	public function index($value='')
+	public function index()
 	{
 		// 是否为 青春校园版 ：0为否
 		$data['flag'] = 0;
 		return view('',$data);
 	}
 	// 欢迎页面
-	public function welcome($value='')
+	public function welcome()
 	{
-		return view();
+		// 统计 注册用户数量 、 导入试题数量 信息
+		$data['userCount'] = Users::count();
+		$data['questionsCount'] = Questions::count();
+		$data['examCount'] = Exam::count();
+		$data['manage'] = session('manage');
+		$data['server'] = input('server.');
+		return view('',$data);
 	}
 	// 待开发页面
-	public function develop($value='')
+	public function develop()
 	{
 		return "正在开发中";
 	}
