8cea4a94e61ab1ef5a802eac3e738f916e3beadb
yuesha <2661103378@qq.com>
2019-10-09 17:08:24 +0800

    人员管理-修改功能完成

--- codeBehind/application/admin/controller/UsersController.php
+++ codeBehind/application/admin/controller/UsersController.php
@@ -27,10 +27,20 @@ class UsersController extends adminBaseClass
 		return return_table($data['usersList'],$data['usersListAll']);
 	}
 
-	// 用户添加、修改 页面渲染
-	public function save()
+	// 用户添加 页面渲染
+	public function add()
 	{
-		return view('');
+		return view();
+	}
+
+	// 用户修改 页面渲染
+	public function edit($id)
+	{
+		$userRow = Users::get($id);
+		return view('',[
+			'id' => $id,
+			'userRow' => $userRow,
+		]);
 	}
 
 	// 保存数据
@@ -48,14 +58,28 @@ class UsersController extends adminBaseClass
 			// 如果密码为空则默认密码为123456
 			$data['pw'] = md5(md5("123456"));
 		}else{
-			$data['pw'] = md5(md5($data['pw']));
+			if ($data['pw'] == "000000") {
+				unset($data['pw']);
+			}else{
+				$data['pw'] = md5(md5($data['pw']));
+			}
 		}
 
 		// 处理数据
 		$data['name'] = trim($data['name']);
 		$data['s_num'] = trim($data['s_num']);
 		$data['sex'] = $data['sex']=='男'?'0':'1';
+		// halt($data);
+		if (isset($data['id'])) {// 说明是编辑状态的数据保存
+			$this -> saveEdit($data);
+		}else{
+			$this -> saveAdd($data);
+		}
+	}
 
+	// 新增状态的数据保存
+	private function saveAdd($data)
+	{
 		// 处理编号和身份证号
 		if ($data['s_num']!='') {
 			$user1 = Users::getByS_num($data['s_num']);
@@ -87,6 +111,38 @@ class UsersController extends adminBaseClass
 		return exit_msg("操作成功！",1);
 	}
 
+	// 编辑状态的数据保存
+	private function saveEdit($data)
+	{
+		$userRow = Users::get($data['id']);
+		// 处理编号和身份证号
+		if ($data['s_num']!='') {
+			$user1 = Users::getByS_num($data['s_num']);
+			// 用户输入的 s_num 与本条记录的 s_num 不同
+			// 并且数据库当中已存在相同的 s_num
+			if ($data['s_num'] != $userRow['s_num'] && $user1) {
+				exit_msg("此编号已存在！请重新填写或使用默认编号！");
+			}
+		}else{
+			// 使用原先的编号
+			$data['s_num'] = $userRow['s_num'];
+		}
+		if ($data['card'] != '' && $data['card'] != $userRow['card']) {
+			$user2 = Users::getByCard($data['card']);
+			if ($user2) {
+				exit_msg("此身份证号已存在！请重新填写或删除身份证号！");
+			}
+		}
+		// 修改记录
+		$user = Users::update($data);
+		// 获取 SQL 语句
+		$sql = Users::getLastSql();
+		// 记录操作日志
+		ManageLog::log(1,$sql);
+		return exit_msg("操作成功！",1);
+	}
+
+	// 删除人员信息
 	public function del($id)
 	{
 		// 删除并获取删除 SQL 语句
